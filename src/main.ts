ad54a657f4236199fc212bae1c0fa4af1d1d0fa0
hard-hub <hardprocrastinates@gmail.com>
2025-07-30 15:32:39 +0530

    final

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { enableProfiling } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 //import { HeaderComponent } from './app/header-component/header-component';
 
+enableProfiling();
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
 //bootstrapApplication(HeaderComponent);
